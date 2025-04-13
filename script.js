8dc640a7e905cea976707b734e3070da4db7f180
MARIUM BINTE MUHAMMAD <165379776+marium0908@users.noreply.github.com>
2025-04-13 11:24:16 +0600

    Update script.js

--- script.js
+++ script.js
@@ -19,6 +19,87 @@ const quiz = [
     options: ["Bible", "Quran", "Torah", "Vedas"],
     answer: "Quran"
   }
+  {
+    question: "Who was the first Prophet in Islam?",
+    options: ['Prophet Muhammad (PBUH)', 'Prophet Ibrahim (Abraham)', ' Prophet Adam','Prophet Musa (Moses)'],
+    answer: "Prophet Adam"
+  },
+  {
+    question: "Which city is known as the birthplace of Prophet Muhammad (PBUH)?",
+    options: ['Jerusalem', ' Mecca', 'Madina', 'Mairo'],
+    answer: "Mecca"
+  },
+  {
+    question: "In which battle did Prophet Muhammad (PBUH) first participate as a leader?",
+    options: ['Battle of Uhud', 'Battle of Badr', 'Battle of Khandaq', 'Battle of Hunayn'],
+    answer: "Battle of Badr"
+  },
+  {
+    question: "How many pillars of Islam are there?",
+    options: ['5', '3', '2', '7'],
+    answer: "5"
+  },
+  {
+    question: "What is the festival that marks the end of Ramadan?",
+    options: ['Eid al-Adha', 'Eid al-Fitr', 'Mawlid', 'Haji'],
+    answer: "Eid al-Fitr"
+  },
+  {
+    question: "What do Muslims celebrate during Eid al-Adha?",
+    options: [' The end of Hajj', 'The birth of Prophet Muhammad (PBUH)', 'The sacrifice of Prophet Ibrahim', 'The start of the Islamic New Year'],
+    answer: "Option A10"
+  },
+  {
+    question: "What percentage of their savings are Muslims required to give to charity?",
+    options: ['1%', '2.5%', '10%', '80%'],
+    answer: "2.5%"
+  },
+  {
+    question: "What is the name of the first revelation Angel?",
+    options: ['Jibrail (A.S.) ', 'Mikail (A.S.)', 'Israfil (A.S.)', 'Azrael (A.S.)'],
+    answer: "Jibrail (A.S.) "
+  },
+  {
+    question: "How many Surahs are there in the Quran?",
+    options: ['110', '120', '114', '300'],
+    answer: "Option A13"
+  },
+  {
+    question: "What is the Arabic name of the pilgrimage to Mecca?",
+    options: ['Zakat', 'Haji', 'Fasting', 'Sadaqah'],
+    answer: "Option A14"
+  },
+  {
+    question: "What is the name of Prophet Muhammad's (PBUH) mother?",
+    options: ['Halima', 'Amina', 'Fatema', 'Asia'],
+    answer: "Option A15"
+  },
+  {
+    question: " What is the Arabic term for charity in Islam?",
+    options: ['Zakat', 'Sawm', 'Haji', 'Salat'],
+    answer: "Zakat"
+  },
+  {
+    question: " What is the name of the Prophet Muhammad’s (PBUH) first wife??",
+    options: ['Aisha(R.A.)', 'Khadija(R.A.)', 'Zainab(R.A.)', 'Fatima(R.A.)'],
+    answer: "Option A17"
+  },
+  {
+    question: "What does the word "Islam" mean?",
+    options: ['Peace', 'Submittion', 'Guidance', 'Broterhood'],
+    answer: "Submittion"
+  },
+  {
+    question: "What is the name of the Islamic greeting?",
+    options: ['Solom', 'Hola', 'Salam', 'Adab'],
+    answer: "Salam"
+  },
+  {
+    question: "What does the word "Zakat" mean in English?",
+    options: ['Charity', 'Fasting', 'Prayer', 'Suplication'],
+    answer: "Charity"
+  },
+  
 ];
 
 let currentQuestion = 0;
